cb75c234805faa65b524f0d0add83882fd5f304d
surrbsk10 <sureshsurya1096@gmail.com>
2019-08-30 17:05:45 +0530

    Latest changes in App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          The App changes will be visible here<code>src/App.js</code> and save to reload.
+          TThe very recent demo changes<code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
